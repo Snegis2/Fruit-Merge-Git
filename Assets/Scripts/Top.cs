5d035318328814d24b8eacbf128d6a5db9a07ea8
Snegis2 <aivarasdabrys@outlook.com>
2024-02-25 17:22:43 +0200

    upload

---
+++ Assets/Scripts/Top.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Top : MonoBehaviour
+{
+    public GameObject GameOverView;
+    public Text ScoreText;
+    void Start()
+    {
+        GameOverView.SetActive(false);
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag.Contains("Fruit"))
+        {
+            if((int)GameManager.gameManager.gameState < (int)GameState.GameOver)
+            {
+                if(collision.gameObject.GetComponent<Fruit>().fruitState == FruitState.Collision)
+                {
+                    GameManager.gameManager.gameState = GameState.GameOver;
+                    Invoke(nameof(ShowGameOverView), 0.5f);
+                    
+                }
+            }
+        }
+    }
+    private void ShowGameOverView()
+    {
+        GameOverView.SetActive(true);
+        ScoreText.text = "Score: " + GameManager.gameManager.score;
+    }
+    
+}
